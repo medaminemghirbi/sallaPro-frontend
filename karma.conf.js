d8bea6a5a40f7d2d9a65bf94c0710f8f7773ce39
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2025-12-19 10:09:52 +0100

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pfe2025-front'),
+      dir: require('path').join(__dirname, './coverage/farhatn'),
       subdir: '.',
       reporters: [
         { type: 'html' },
